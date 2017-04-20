ecc984514094c1bcea06db7c46379d72c5ea8b9a
LukasWuste <lukasw11@web.de>
2017-04-20 14:45:03 +0200

    Buchungsmodul hinzugefügt

---
+++ Modul_Buchung.java
@@ -0,0 +1,24 @@
+import java.util.*;
+/** PROGRAMMKOPF
+  * Modul Buchung
+  *
+  * @version 1.0 vom 20.04.2017
+  * 
+  * Klasse: FS 63
+  * Name: Lukas Wuestenhagen
+  *
+  PROGRAMMKOPF ENDE*/
+
+public class Modul_Buchung {  
+  public static void main(String[] args) {  // Beginn der Hauptfunktion
+    int Auswahl;
+    int Wohnung = 10;
+    int Kundennummer = 365; //Fuer jeden Tag eine Kundenummer
+    String Buchungsdaten[ ][ ] = new String[Wohnung][Kundennummer]; //Vorname, Nachname, Adresse
+    do {
+      
+    } while (Auswahl != 5); // end of do-while
+    
+    //return Buchungsdaten;
+  } // Ender der Hauptfunktion
+} // end of class EAngestellte
