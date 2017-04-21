fa6efb77bc6768ace64bbfb419261f62fa8696c0
xAffectum <nico.fischer98@live.de>
2017-04-21 12:30:49 +0200

    Modul_Update hinzugefügt

--- Modul_Update2.java
+++ Modul_Update2.java
@@ -1,125 +1,127 @@
  import java.io.*;
 public class Modul_Update2 {
+  
+  public static void read(String fwhg[][],String kdata[][],String bdata[][],int jahr){
+    String dataFwhg = "db/fwohnungen.txt", dataKunden = "db/kundendaten.txt", dataBuchung = "db/buchungsdaten_"+jahr+".txt";
+    BufferedReader br = null;
+    try {
+      br = new BufferedReader(new FileReader(new File(dataFwhg)));
+      String line = null;
+      for (int i=0;(line = br.readLine()) != null ;i++ ) {
+        String[] parts = line.split(";"); 
+        for (int l = 0;l<fwhg[i].length;l++ ) {
+          fwhg[i][l] = parts[l];
+        } // end of for
+      } // end of for
+    } catch(FileNotFoundException e) {
+      e.printStackTrace();
+    } catch(IOException e) {
+      e.printStackTrace();
+    }
     
-  public static void update(char opt,String fwhg[][],String kdata[][],String bdata[][]){
-    switch (opt) {
-      case 'g': 
-      BufferedReader br = null;
-      try {
-        br = new BufferedReader(new FileReader(new File("fwohnungen.txt")));
-        String line = null;
-        for (int i=0;(line = br.readLine()) != null ;i++ ) {
-          String[] parts = line.split(";"); 
-          for (int l = 0;l<fwhg[i].length;l++ ) {
-            fwhg[i][l] = parts[l];
-          } // end of for
+    br = null;
+    try {
+      br = new BufferedReader(new FileReader(new File(dataKunden)));
+      String line = null;
+      for (int i=0;(line = br.readLine()) != null ;i++ ) {
+        String[] parts = line.split(";"); 
+        for (int l = 0;l<kdata[i].length;l++ ) { 
+          kdata[i][l] = parts[l];
         } // end of for
-      } catch(FileNotFoundException e) {
-        e.printStackTrace();
-      } catch(IOException e) {
-        e.printStackTrace();
+      } // end of for
+    } catch(FileNotFoundException e) {
+      e.printStackTrace();
+    } catch(IOException e) {
+      e.printStackTrace();
+    } 
+    
+    br = null;
+    try {
+      int whg;
+      int tag;
+      br = new BufferedReader(new FileReader(new File(dataBuchung)));
+      String line = null;
+      for (int i=0;(line = br.readLine()) != null ;i++ ) {
+        String[] parts = line.split(";"); 
+        whg = Integer.parseInt(parts[0]);
+        tag = Integer.parseInt(parts[1]); 
+        bdata[whg][tag] = parts[2];
+      } // end of for
+    } catch(FileNotFoundException e) {
+      //e.printStackTrace();
+      System.out.println("Fuer das Jahr "+jahr+" bestehen bisher noch keine Buchungen!");
+    } catch(IOException e) {
+      //e.printStackTrace();
+    }    
+    
+  }
+  
+  
+  public static void write(String fwhg[][],String kdata[][],String bdata[][],int jahr){
+    String dataFwhg = "db/fwohnungen.txt", dataKunden = "db/kundendaten.txt", dataBuchung = "db/buchungsdaten_"+jahr+".txt";
+    try {
+      BufferedWriter writer = new BufferedWriter(new FileWriter(dataFwhg));
+      for (int i = 0; i < fwhg.length; i++) {
+        for (int j = 0; j < fwhg[i].length; j++) {
+          if (fwhg[i][j] != null) {
+            writer.write(fwhg[i][j] + ";");  
+          } // end of if 
+        }
+        if (fwhg[i][1] != null) {
+          writer.newLine(); 
+        } // end of if
       }
+      writer.close();
       
-      br = null;
-      try {
-        br = new BufferedReader(new FileReader(new File("kundendaten.txt")));
-        String line = null;
-        for (int i=0;(line = br.readLine()) != null ;i++ ) {
-          String[] parts = line.split(";"); 
-          for (int l = 0;l<kdata[i].length;l++ ) { 
-            kdata[i][l] = parts[l];
-          } // end of for
-        } // end of for
-      } catch(FileNotFoundException e) {
-        e.printStackTrace();
-      } catch(IOException e) {
-        e.printStackTrace();
-      } 
+    }
+    catch (IOException e) {
+      e.printStackTrace();
+    }
+    
+    try {
+      BufferedWriter writer = new BufferedWriter(new FileWriter(dataKunden));
+      for (int i = 0; i < kdata.length; i++) {
+        for (int j = 0; j < kdata[i].length; j++) {
+          if (kdata[i][j] != null) {
+            writer.write(kdata[i][j] + ";"); 
+          } // end of if  
+        } 
+        if (kdata[i][1] != null) {
+          writer.newLine(); 
+        } // end of if
+      }
+      writer.close();
       
-      br = null;
-      try {
-        int whg;
-        int tag;
-        br = new BufferedReader(new FileReader(new File("buchungsdaten.txt")));
-        String line = null;
-        for (int i=0;(line = br.readLine()) != null ;i++ ) {
-          String[] parts = line.split(";"); 
-          whg = Integer.parseInt(parts[0]);
-          tag = Integer.parseInt(parts[1]); 
-          bdata[whg][tag] = parts[2];
-        } // end of for
-      } catch(FileNotFoundException e) {
-        e.printStackTrace();
-      } catch(IOException e) {
-        e.printStackTrace();
-      }   
-      break;
-      case 's': 
-      try {
-        BufferedWriter writer = new BufferedWriter(new FileWriter("fwohnungen.txt"));
-        for (int i = 0; i < fwhg.length; i++) {
-          for (int j = 0; j < fwhg[i].length; j++) {
-            if (fwhg[i][j] != null) {
-              writer.write(fwhg[i][j] + ";");  
-            } // end of if 
-          }
-          if (fwhg[i][1] != null) {
+    }
+    catch (IOException e) {
+      e.printStackTrace();
+    }
+    
+    try {
+      BufferedWriter writer = new BufferedWriter(new FileWriter(dataBuchung));
+      for (int i = 0; i < bdata.length; i++) {
+        for (int j = 0; j < bdata[i].length; j++) {
+          if (bdata[i][j] != null) {
+            writer.write(i + ";");
+            writer.write(j + ";");
+            writer.write(bdata[i][j] + ";");
+          } // end of if  
+          if (bdata[i][j] != null) {
             writer.newLine(); 
           } // end of if
-        }
-        writer.close();
+        } 
         
       }
-      catch (IOException e) {
-        e.printStackTrace();
-      }
+      writer.close();
       
-      try {
-        BufferedWriter writer = new BufferedWriter(new FileWriter("kundendaten.txt"));
-        for (int i = 0; i < kdata.length; i++) {
-          for (int j = 0; j < kdata[i].length; j++) {
-            if (kdata[i][j] != null) {
-              writer.write(kdata[i][j] + ";"); 
-            } // end of if  
-          } 
-          if (kdata[i][1] != null) {
-            writer.newLine(); 
-          } // end of if
-        }
-        writer.close();
-        
-      }
-      catch (IOException e) {
-        e.printStackTrace();
-      }
-      
-      try {
-        BufferedWriter writer = new BufferedWriter(new FileWriter("buchungsdaten.txt"));
-        for (int i = 0; i < bdata.length; i++) {
-          for (int j = 0; j < bdata[i].length; j++) {
-            if (bdata[i][j] != null) {
-              writer.write(i + ";");
-              writer.write(j + ";");
-              writer.write(bdata[i][j] + ";");
-            } // end of if  
-            if (bdata[i][j] != null) {
-              writer.newLine(); 
-            } // end of if
-          } 
-          
-        }
-        writer.close();
-        
-      }
-      catch (IOException e) {
-        e.printStackTrace();
-      }
-      
-      break;
-      default: 
-      System.out.println("Fehler!");
-      
-    } // end of switch
-  } 
+    }
+    catch (IOException e) {
+      e.printStackTrace();
+    }
+    
+    
+  }
+  
+  
   
 } // end of class Modul_Update2
