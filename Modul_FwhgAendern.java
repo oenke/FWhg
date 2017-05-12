5d5fdca6b0de3eea4eeb0f0b845fbb497cf7bd31
oenke <taxonomie@gmx.de>
2017-05-12 11:49:30 +0200

    Fehlerabfangen hinzugefügt im Modul FwhgAendern

--- Modul_FwhgAendern.java
+++ Modul_FwhgAendern.java
@@ -10,35 +10,81 @@
 
 public class Modul_FwhgAendern {
   
-  public static String[][] aendern(String fwhg[][]) {
+  public static void aendern(String fwhg[][]) {
     
     int wahl = 0; 
     String s;
     
+    System.out.println("\n____________________Managementsystem fuer Ferienwohnungen_______________________");
+    System.out.println("                        Aendern der Ferienwohnungen\n");
+    
+    //Tabellenkopf
     System.out.printf("%3s | %10s | %8s" , s="ID", s="Tagespreis", s="Groesse");
     System.out.printf("\n%s%s%s%s" , s="-------",s,s,s);
     
+    // Ausgabe einer Tabelle
     for (int i = 0; i <= 9 ; i++ ) {
       
-      System.out.printf("\n%3s | %10s | %8s" , s=fwhg[i][0], s=fwhg[i][1] + " Euro", s=fwhg[i][2] + " qm");
+      System.out.printf("\n%3s | %8.2f Euro | %6.2f qm" , s=fwhg[i][0], Float.parseFloat(fwhg[i][1]), Float.parseFloat(fwhg[i][2]));
       //System.out.print("\n" + fwhg[i][0] + " " + fwhg[i][1] + " " + fwhg[i][2]); 
       
     } // end of for
     
-    System.out.println("\nWelche Ferienwohnung moechten Sie aender? ");
-    
-    wahl = (Tastatur.liesInt() -1);
-    
-    System.out.println("Geben Sie bitte den neuen Preis ein: "); 
-    
-    fwhg[wahl][1] = String.valueOf(Tastatur.liesInt());
-    
-    System.out.println("Geben Sie bitte die neue Groesse ein: "); 
-    
-    fwhg[wahl][2] = String.valueOf(Tastatur.liesInt());
-    
-    return fwhg;
-    
+    //Abfangen von Fehleingaben und setzen neuer Werte
+    boolean wdh = false;
+    do {
+      wdh = false;
+      System.out.println("\n\nWelche Ferienwohnung moechten Sie aendern? ");
+      System.out.println("Zum abbrechen (0) druecken");
+      
+      wahl = (Tastatur.liesInt() -1);
+      float x;
+      if (wahl >= 0 && wahl < 10) {
+        do {
+          x = 0;
+          wdh = false;
+          System.out.println("Geben Sie bitte den neuen Preis ein: "); 
+          x = Tastatur.liesFloat();
+          wdh = Modul_FwhgAendern.fehler(x);
+        } while (wdh==true); // end of do-while
+        
+        fwhg[wahl][1] = String.valueOf(x);
+        do {
+          x = 0;
+          wdh = false;
+          System.out.println("Geben Sie bitte die neue Groesse ein: ");
+          x = Tastatur.liesFloat();
+          wdh = Modul_FwhgAendern.fehler(x);
+        } while (wdh==true); // end of do-while 
+        fwhg[wahl][2] = String.valueOf(x);
+        
+      } // end of if
+      
+      else if (wahl < 0) {
+      }
+      
+      else {
+        
+        System.out.println("Fehler bei der Eingabe, bitte versuchen Sie es erneut.");
+        wdh = true;
+        
+      } // end of if-else
+      
+    } while (wdh==true); // end of do-while
     
   }
+  
+  public static boolean fehler(double x) {
+    boolean wdh = false;
+    if (x < 0) {
+      System.out.println("Fehleingabe");
+      wdh = true;
+    } // end of if
+    else {
+      wdh = false;
+    } // end of if-else
+    
+    return wdh;
+  }
+  
 }
