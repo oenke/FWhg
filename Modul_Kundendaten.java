7d9f4f3aa09418ef453b838b6066cbf36de0f39e
LukasWuste <lukasw11@web.de>
2017-05-17 15:19:14 +0200

    Kudendaten, Kommentierung und Leichte Änderung

--- Modul_Kundendaten.java
+++ Modul_Kundendaten.java
@@ -10,21 +10,21 @@ import java.util.*;
   PROGRAMMKOPF ENDE*/
 
 public class Modul_Kundendaten {
-  public static void KundenEingabe(String[][] Kundendaten, int AnzahlKunden, int Eintrage) {  // Beginn der Hauptfunktion
+  public static void KundenEingabe(String[][] Kundendaten, int AnzahlKunden, int Eintrage) {  // Eingabe neuer Kundendaten in einem 
     if (Eintrage >= AnzahlKunden) {
       System.out.print("Zu viele Eintraege.");
     } // end of if
     else {
       int k = 0;
       while (Kundendaten[k][0] != null) {            
-        k++;
+        k++;                                                                                 //KundenID erhöhen solange KundeID schon vorhanden ist
       } // end of while
       
       for (int i=0;i<3;i++) {
-        switch (i) {
+        switch (i) {                                                                         //Eingabe der relevanten Daten
           case  0: 
           System.out.print("Bitte geben Sie den Vornamen ein: ");
-          Kundendaten[k][i] = Tastatur.liesString();
+          Kundendaten[k][i] = Tastatur.liesString();                                         
           break;
           case  1: 
           System.out.print("Bitte geben Sie den Nachnamen ein: ");
@@ -43,20 +43,20 @@ public class Modul_Kundendaten {
     } // end of if-else
   } 
   
-  public static void KundenVeraender(String[][] Kundendaten, int AnzahlKunden, int Eintrage) {  // Beginn der Hauptfunktion
+  public static void KundenVeraender(String[][] Kundendaten, int AnzahlKunden, int Eintrage) {  //Veraenderung von Kundendaten
     System.out.print("Bitte geben Sie den Datensatz ein den Sie veraendern wollen: ");
     int Ausgabewahl2 = Tastatur.liesInt()-1;
     if (Ausgabewahl2 < 0 || Ausgabewahl2 > AnzahlKunden) {
-      System.out.println("Datensatz nicht vorhanden.");
+      System.out.println("Datensatz nicht vorhanden.");                                          //Fehlerausgabe bei KundeID's auersserhalb des Spektrums
     } // end of if
     else {
       if (Kundendaten[Ausgabewahl2][0] != null) {
         for (int i=0;i<3 ;i++ ) {
-          System.out.println("\n"+(Ausgabewahl2+1)+". "+Kundendaten[Ausgabewahl2][i]);
+          System.out.println("\n"+(Ausgabewahl2+1)+". "+Kundendaten[Ausgabewahl2][i]);           //Bereits vorhandene Daten printen
         } // end of for
       } // end of if
       for (int i=0;i<3;i++) {
-        switch (i) {
+        switch (i) {                                                                             //Eingabe der relevanten Daten
           case  0: 
           System.out.print("Bitte geben Sie den Vornamen ein: ");
           Kundendaten[Ausgabewahl2][i] = Tastatur.liesString();
@@ -76,49 +76,49 @@ public class Modul_Kundendaten {
     }
   }
   
-  public static void AusgabeEinzel(String[][] Kundendaten, int AnzahlKunden, int Eintrage) {  // Beginn der Hauptfunktion
+  public static void AusgabeEinzel(String[][] Kundendaten, int AnzahlKunden, int Eintrage) {  //Einzelne Ausgabe eines Kundendatensatzes
     System.out.print("Bitte geben Sie den Datensatz ein: ");
     int Ausgabewahl = Tastatur.liesInt()-1;
     if (Ausgabewahl < 0 || Ausgabewahl > AnzahlKunden) {
-      System.out.println("Datensatz nicht vorhanden.");
+      System.out.println("Datensatz nicht vorhanden.");                                       //Fehlerausgabe bei KundeID's auersserhalb des Spektrums
     } // end of if
     else {
       for (int i=0;i<3 ;i++ ) {
-        System.out.println("\n"+(Ausgabewahl+1)+". "+Kundendaten[Ausgabewahl][i]);
+        System.out.println("\n"+(Ausgabewahl+1)+". "+Kundendaten[Ausgabewahl][i]);            //Ausgabe des Datensatzes
       } // end of for
     } // end of if-else
   }
   
-  public static void AusgabeAlle(String[][] Kundendaten, int AnzahlKunden, int Eintrage) {  // Beginn der Hauptfunktion
+  public static void AusgabeAlle(String[][] Kundendaten, int AnzahlKunden, int Eintrage) {  // Ausgabe aller Kundendatensaetze
     System.out.println();
     for (int t=0;t<AnzahlKunden; t++) {
       if (Kundendaten[t][0] != null) {
         for (int i=0;i<3;i++) {
-          System.out.println((t+1)+". "+Kundendaten[t][i]);
+          System.out.println((t+1)+". "+Kundendaten[t][i]);                                 // Ausgabe der Datensaetze mittels Schleife
         } // end of for
         System.out.println("____________________");
       } // end of if
     } // end of for
   }
   
-  public static void BuchungDerKundenID(String[][] Kundendaten, int AnzahlKunden, int Eintrage, String[][] Buchungsdaten ) {  // Beginn der Hauptfunktion
+  public static void BuchungDerKundenID(String[][] Kundendaten, int AnzahlKunden, int Eintrage, String[][] Buchungsdaten ) {  // Ueberpruefung der Buchungen eines Kunden mittels seiner ID
     System.out.print("Bitte geben Sie die KundenID ein: ");
     int Eingabe = Tastatur.liesInt();
-    boolean Treffer = false;
+    boolean Treffer = false;                                                                                                           //Bei einem Treffer zur Ausgabe verschiedener Texte
     if (Eingabe > 0 && Eingabe < AnzahlKunden) {
       for (int i=0;i<Buchungsdaten.length;i++ ) {
         for (int k = 0;k<Buchungsdaten[i].length;k++ ) { 
           if (Buchungsdaten[i][k] != null && Integer.parseInt(Buchungsdaten[i][k]) == Eingabe ) {
-            System.out.println("Die Wohnung " + i + " am " + k + ". Tag.");
-            Treffer = true; 
+            System.out.println("Die Wohnung " + i + " am " + k + ". Tag.");                                                            //Durchlaufen des Zweidimensionalen Arrays nach Wohnungen und Tagen
+            Treffer = true;                                                                                                            //Bis eine KundenID gefunden wurde die mit der Eingabe passt, dann printen.
           }
         } // end of for
       } // end of for
       if (Treffer) {
-        System.out.println("\nDiese Tage wurden vom Kunden "+ Kundendaten[Eingabe][0] + " " + Kundendaten[Eingabe][1] + " gebucht.");
+        System.out.println("\nDiese Tage wurden vom Kunden "+ Kundendaten[Eingabe][0] + " " + Kundendaten[Eingabe][1] + " gebucht.");  //Bei einem Treffer wird der name geprinted
       } // end of if
       else {
-        System.out.println("\nDer Kunde mit der ID:" + Eingabe + " hat bisher noch nichts gebucht.");
+        System.out.println("\nDer Kunde mit der ID: " + Eingabe + " hat bisher noch nichts gebucht.");                                  //Bei keinem Treffer wird nur die ID erneut ausgegeben
       } // end of if-else
     } // end of if
     else {
@@ -138,7 +138,7 @@ public class Modul_Kundendaten {
       System.out.println("6 - Zurueckkehren zum Hauptprogramm.");
       Auswahl = Tastatur.liesInt(); 
       
-      switch (Auswahl) {
+      switch (Auswahl) {                                                                         //Verschiedene Cases fuer unterschiedliche Untermodule
         case  1:
         Modul_Kundendaten.KundenEingabe(Kundendaten,AnzahlKunden,Eintrage);
         break;
